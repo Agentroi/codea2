391079d8e737b792f88ac80b8376b8a780448e01
agentroi <roidalsgard@gmail.com>
2021-10-13 13:23:16 +0200

    ProgramCounter works a little

--- src/main/scala/ProgramCounter.scala
+++ src/main/scala/ProgramCounter.scala
@@ -8,8 +8,23 @@ class ProgramCounter extends Module {
     val run = Input(Bool())
     val programCounterJump = Input(UInt(16.W))
     val programCounter = Output(UInt(16.W))
-  })
 
+  })
   //Implement this module here (respect the provided interface, since it used by the tester)
 
+  val pcReg = RegInit(0.U(16.W))
+  //val pcjReg = RegInit(0.U(16.W));
+  when(!io.run) {
+    pcReg := pcReg
+  }
+  when(io.stop) {
+    pcReg := pcReg
+  }
+  when(io.run && !io.stop && !io.jump) {
+    pcReg := pcReg + 1.U
+  }
+  when(io.run && !io.stop && io.jump) {
+    pcReg := io.programCounterJump
+  }
+  io.programCounter := pcReg
 }
